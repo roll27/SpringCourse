3cf341edf71fd15a1a2cec9de18d4b6ad7be430b
roll27 <roll27@126.com>
2017-09-18 16:36:45 +0800

    spring自动装配

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages="soundsystem")   //组件扫描指定包。
 public class CDPlayerConfig {
 
 }
--- src/main/java/soundsystem/SgtPeppers.java
+++ src/main/java/soundsystem/SgtPeppers.java
@@ -2,7 +2,7 @@ package soundsystem;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("lonelyHeartsClub")	//注解bean，并为bean命名。
 public class SgtPeppers implements CompactDisc{
 
 	public void play() {
